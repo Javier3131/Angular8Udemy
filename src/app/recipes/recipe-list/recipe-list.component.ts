39e0a0b7889c0e29dc31571e5c32a2a08d0e0883
Javier Lopez <javierl.1994@gmail.com>
2019-09-02 20:11:09 -0600

    Initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipes } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipes[] = [
+    new Recipes("Recipe Name", "Recipe Description", "https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32.jpg")
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
