a50bc3853a44cd1034edfccc6c930c034abcef85
JavLop <javierl.1994@gmail.com>
2019-10-25 17:42:18 -0600

    Setting up firebase communication.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,25 +9,27 @@ export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
     recipeSelected = new EventEmitter<Recipe>();
 
-    private recipes: Recipe[] = [
-        new Recipe("Crazy Pizza",
-            "A pizza that will make you go crazy",
-            "https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32.jpg",
-            [
-                new Ingredient('pasta', 2),
-                new Ingredient('another ingredient', 4)
-            ]
-        ),
-        new Recipe("Pasta",
-            "A pasta",
-            "https://ep01.epimg.net/elcomidista/imagenes/2018/06/26/receta/1530022065_262773_1530523801_media_normal.jpg",
-            [
-                new Ingredient('cheese', 5),
-                new Ingredient('another ingredient', 1)
-            ]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe("Crazy Pizza",
+    //         "A pizza that will make you go crazy",
+    //         "https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32.jpg",
+    //         [
+    //             new Ingredient('pasta', 2),
+    //             new Ingredient('another ingredient', 4)
+    //         ]
+    //     ),
+    //     new Recipe("Pasta",
+    //         "A pasta",
+    //         "https://ep01.epimg.net/elcomidista/imagenes/2018/06/26/receta/1530022065_262773_1530523801_media_normal.jpg",
+    //         [
+    //             new Ingredient('cheese', 5),
+    //             new Ingredient('another ingredient', 1)
+    //         ]
+    //     )
+    // ];
 
+    private recipes: Recipe[] = [];
+    
     constructor(private slService: ShoppingListService) { }
 
     getRecipes() {
@@ -36,6 +38,11 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngridients(ingredients);
+    }
+
+    setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
     }
 
     getRecipe(id: number) {
