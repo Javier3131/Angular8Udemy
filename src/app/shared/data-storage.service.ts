d0093143cc12e0ace6f1765d038852c09b22566f
Javier Lopez <javierl.1994@gmail.com>
2019-10-28 00:27:03 -0600

    Implementing resolver. Starting auth module.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -24,7 +24,7 @@ export class DataStorageService {
 
     fetchRecipes() {
         const endPoint: string = 'recipes.json';
-        this.http
+        return this.http
             .get<Recipe[]>(this.url + endPoint)
             .pipe(
                 map(recipes => {
@@ -35,9 +35,10 @@ export class DataStorageService {
                             ingredients: recipe.ingredients ? recipe.ingredients : []
                         };
                     });
-                }))
-            .subscribe(recipes => {
-                this.recipeService.setRecipes(recipes);
-            });
+                }),
+                tap(recipes => {
+                    this.recipeService.setRecipes(recipes);
+                })
+            );
     }
 }
